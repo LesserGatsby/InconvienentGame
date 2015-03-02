db075c9de02d08f41ee9ee3a2173fac1a9f6759b
Zachary Spratt <zachman331@gmail.com>
2015-03-02 14:50:12 -0800

    Fin

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.mygdx.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-                config.width = 800;
-                config.height = 800;
+                config.width = 1000;
+                config.height = 700;
 		new LwjglApplication(new Game(), config);
 	}
 }
